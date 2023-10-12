7898e64f629aabb31700aa70c3516a65c66d6b63
Ben Ross <benjamin.ross@youview.com>
2023-10-12 15:13:22 +0100

    Lesson 525: Sending HTTP Requests
    - When adding an ingredient, add ingredient to the backend NoSQL Firebase Realtime Database. If succeeds, add to local ingredient list in front-end.

--- hooks-01-starting-project/src/components/Ingredients/Ingredients.js
+++ hooks-01-starting-project/src/components/Ingredients/Ingredients.js
@@ -11,9 +11,23 @@ function Ingredients() {
 
   // Create a function for updating the list of ingredients that you can then pass to the form
   function addIngredient(ingredient) {
-    // Pass in an updater function as an argument to setState to calculate the next state based on the previous state
-    setIngredientsState(prevIngredients => {
-      return [...prevIngredients, {id: Math.random().toString(), ...ingredient}]
+    // Add the new ingredient to the backend (Firebase NoSQL Realtime Database)
+    fetch("https://academind-react-ingredie-2ed3a-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(ingredient)
+    })
+    // Wait for that request to resolve
+    // Parse the request body as JSON
+    .then(response => response.json())
+    .then((json) => {
+      // Update the local ingredients list React state with the new ingredient
+      // Pass in an updater function as an argument to setState to calculate the next state based on the previous state
+      setIngredientsState(prevIngredients => {
+        return [...prevIngredients, {id: json.name, ...ingredient}]
+      });
     });
   }
 
